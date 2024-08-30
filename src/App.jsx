d04ae2b34f06775d0e28bb509b10c21bbae16b6a
Olorunfemi Olamide <166822505+BigOTF@users.noreply.github.com>
2024-08-30 13:21:43 +0100

    Added the SubHero page also did the mobile screen but will be pending based on further review

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
+import SubHero from './components/SubHero/SubHero'
 
 function App() {
 
   return (
-    <div>
+    <div className='pb-[500px]'>
       <Header />
       <Hero />
+      <SubHero />
     </div>
   )
 }
