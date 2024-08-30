8b11c4938f43c1a3d3549aa26ba0c7d2d064b879
Olorunfemi Olamide <166822505+BigOTF@users.noreply.github.com>
2024-08-30 14:54:27 +0100

    Added the link for the about

--- src/components/AboutUs/AboutUsText.jsx
+++ src/components/AboutUs/AboutUsText.jsx
@@ -10,7 +10,7 @@ const AboutUsText = () => {
 
         <p className='md:text-[20px] text-[#363247] md:leading-[27.28px]'>CRida is your reliable transportation partner, dedicated to ensuring safe and comfortable journeys. We create value by being proactive in maintaining quality fleet at reduced costs in order to provide efficient services by making sure we are at your service when needed. Meet our team and discover our commitment to excellence.</p>
 
-        <a className='md:w-[189px] md:h-[49px] md:text-[24px] leading-[32.74px] p-[8px]  bg-[#5810BF] text-white rounded-lg cursor-pointer'>
+        <a href='/about' className='md:w-[189px] md:h-[49px] md:text-[24px] leading-[32.74px] p-[8px]  bg-[#5810BF] text-white rounded-lg cursor-pointer'>
             More About Us
         </a>
     </div>
