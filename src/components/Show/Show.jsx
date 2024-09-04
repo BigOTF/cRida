498ea066837637428ff3c630103b593f43c1f495
Olorunfemi Olamide <166822505+BigOTF@users.noreply.github.com>
2024-09-04 20:58:11 +0100

    Created the section on the Show

--- src/components/Show/Show.jsx
+++ src/components/Show/Show.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Show = () => {
   return (
-    <div className='w-[429px] h-[184px] mt-[60px] md:w-[1440px] md:h-[496px] md:mt-[100px] afterExploreBackground'></div>
+    <div className=''>
+        <div className='w-[429px] h-[184px] mt-[60px] md:w-[1440px] md:h-[496px] md:mt-[100px] afterExploreBackground'></div>
+        <div className='mt-[20px] w-[368px] h-[99px] mx-[30px] md:mt-[20px] md:w-[687px] md:h-[110px] md:ml-[100px]'>
+            <p className='text-[24px] leading-[32.74px] md:text-[40px] md:font-bold md:leading-[54.56px] text-[#070125] md:underline flex flex-col'>CRida Services <span className='text-[24px] md:text-[40px] font-bold leading-[32.74px] md:leading-[54.56px]'>Available all over Abuja F.C.T, Nigeria</span></p>
+        </div>
+        <p className='text-[20px] mt-[20px] w-[368px] h-[135px] mx-[30px] leading-[27.28px] text-[#363247] md:hidden'>Need a safe and convenient way to get around Abuja? Look no further than CRida Services! We offer a variety of transportation options to fit your needs, all across the Federal Capital Territory.</p>
+    </div>
   )
 }
 
