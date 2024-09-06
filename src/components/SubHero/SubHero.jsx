0fc7123a2b92b15cf7c025dcbf6a8896713bab9f
Olorunfemi Olamide <166822505+BigOTF@users.noreply.github.com>
2024-09-06 23:26:06 +0100

    Made adjustment to the previous changes

--- src/components/SubHero/SubHero.jsx
+++ src/components/SubHero/SubHero.jsx
@@ -4,7 +4,7 @@ const SubHero = () => {
   return (
     <div className='bg-[#DEE7F5] w-full'>
 
-       <div className='bg-white w-[368px] h-[65px] md:w-[1030px] md:h-[131px] flex mx-auto justify-around items-center gap-[8px] md:gap-[40px] transform md:translate-y-[-50%] translate-y-[-80px]'>
+       <div className='bg-white w-[368px] h-[65px] mx-[30px] md:mx-auto md:w-[1030px] md:h-[131px] flex justify-around items-center gap-[8px] md:gap-[40px] transform md:translate-y-[-50%] translate-y-[-80px]'>
           <div className='flex flex-col md:gap-[8px] w-[62px] h-[49px] md:w-[219.5px] md:h-[83px] items-center'>
               <p className='nunito-700 text-[#070125] md:text-[32px] text-[20px] md:leading-[43.65px] leading-[27.28px]'>150+</p>
               <p className='text-[#363247] nunito-400 md:text-[20px] text-[16px] text-center md:leading-[27.28px] leading-[21.82px]'>Cars</p>
