e5ef3fc7244e6e83f237c4770fb5c845c8dc65bc
Olorunfemi Olamide <166822505+BigOTF@users.noreply.github.com>
2024-09-06 23:29:37 +0100

    made more

--- src/components/Hero/HeroSide.jsx
+++ src/components/Hero/HeroSide.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HeroSide = () => {
   return (
-    <div className='text-white flex flex-col gap-[16px] w-[368px] mx-[30px] md:mx-0 h-[283px] mt-[45px] md:mt-0 md:w-[525px] md:h-[331px]'>
+    <div className='text-white flex flex-col gap-[16px] w-full mx-[30px] md:mx-0 h-[283px] mt-[45px] md:mt-0 md:w-[525px] md:h-[331px]'>
         <div className='flex items-center gap-[8px]'>
           <div className='w-[60px] border-[#5810BF] border-[3px]'></div>
           <p className='md:text-[24px] text-[20px] nunito-400'>ENJOY YOUR RIDE</p>
