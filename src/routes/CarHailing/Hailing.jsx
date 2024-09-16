bde5ef3a4e6295a62b64fd90b7f4dbfc306480d8
Olorunfemi Olamide <166822505+BigOTF@users.noreply.github.com>
2024-09-16 14:58:31 +0100

    Started the WhyHailing

--- src/routes/CarHailing/Hailing.jsx
+++ src/routes/CarHailing/Hailing.jsx
@@ -2,14 +2,18 @@ import React, { useState } from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 import HailingHero from './HailingHero'
+import HailingSubHero from './HailingSubHero'
+import WhyHailing from './WhyHailing'
 
 const Hailing = () => {
   return (
-    <div>
+    <div className='bg-[#DEE7F5]'>
       <Header />
 
         <div className='w-full'>
           <HailingHero />
+          <HailingSubHero />
+          <WhyHailing />
          
         </div>
       <Footer />
---
+++ src/routes/CarHailing/WhyHailing.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const WhyHailing = () => {
+  return (
+    <div className='border-4 border-red-500 w-full'>
+
+        <div className='border-4 border-blue-500'>
+
+            <div className='border-4 border-green-500 flex flex-col p-[8px] md:gap-[32px]'>
+                <p className='nunito-700 text-[#070125] md:text-[24px] md:leading-[32.74px]'>For Drivers</p>
+                <p className='text-[#363247] nunito-400 text-[20px] leading-[27.28px]'>Make Everyday Payday</p>
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default WhyHailing
