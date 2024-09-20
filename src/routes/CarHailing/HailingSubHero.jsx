8ca808566e063ab9f4342ca91b6778ea1f741532
Olorunfemi Olamide <166822505+BigOTF@users.noreply.github.com>
2024-09-20 17:06:29 +0100

    Adjusted the Hailing subHero page section

--- src/routes/CarHailing/HailingSubHero.jsx
+++ src/routes/CarHailing/HailingSubHero.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const HailingSubHero = () => {
   return (
-    <div className='w-full flex flex-col items-center justify-center mt-[20px] md:px-[350px] px-[10px]'>
+    <div className='w-full flex flex-col items-center justify-center mt-[20px] md:mt-[40px] md:px-[350px] px-[10px]'>
         <div className='flex flex-col items-center gap-[8px]'>
-            <p className='text-[#BF5B12] nunito-700 text-center text-[24px] leading-[32.74px] md:text-[32px] md:leading-[43.65px]'>Drive and Ride with CRida</p>
+            <p className='text-[#BF5B12] nunito-700 text-center text-[24px] leading-[32.74px] md:text-[40px] md:leading-[54.56px]'>Drive and Ride with CRida</p>
             <p className='text-[#363247] nunito-400 text-[20px] leading-[27.28px] text-center'>Choose comfort with our ride-hailing and get a car directly to you. Ride-share and enjoy a friendly atmosphere with passengers heading in the same direction.</p>
         </div>
     </div>
